9524813efcee836b0dcfc224437d72871a7eede8
Nate Lee <nleepercussion@gmail.com>
2021-08-09 12:26:04 -0400

    Added user avatar to nav

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const [userMetadata, setUserMetadata] = useState(null);
+  const { user, isAuthenticated } = useAuth0();
+  // const [userMetadata, setUserMetadata] = useState(null);
 
   return (
     isAuthenticated && (
@@ -11,12 +10,12 @@ const Profile = () => {
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-        <h3>User Metadata</h3>
+        {/* <h3>User Metadata</h3>
         {userMetadata ? (
           <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
         ) : (
           "No user metadata defined"
-        )}
+        )} */}
       </div>
     )
   );
