7de304fa0e5af72613005769d8e682366caa2fb9
Nate Lee <nleepercussion@gmail.com>
2021-08-09 17:10:24 -0400

    Wrapped button in Nav.link, added padding to applied checkbox

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -47,8 +47,9 @@ const Navigation = (props) => {
             </Nav.Link>
           </>
         )}
-        <button id="theme-toggle" type="button" onClick={props.toggleDarkMode}>
-    {props.darkMode ? 
+        <Nav.Link eventKey="4">
+          <button id="theme-toggle" type="button" onClick={props.toggleDarkMode}>
+            {props.darkMode ? 
                             (<svg id="sun" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <circle cx="12" cy="12" r="5"></circle>
                                 <line x1="12" y1="1" x2="12" y2="3"></line>
@@ -65,8 +66,9 @@ const Navigation = (props) => {
                                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
                             </svg>)
                         }
-    </button>
-        
+      </button>
+    </Nav.Link>
+
       </Nav>
     </Navbar.Collapse>
     
